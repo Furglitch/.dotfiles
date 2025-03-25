2711b213c6cf00308d465d704ac7a51c3a908f6b
furglitch <samlchmn@gmail.com>
2025-03-25 18:17:13 -0500

    Add discord logo to .desktop file

--- scripts/discord.sh
+++ scripts/discord.sh
@@ -8,3 +8,6 @@ wget -O "$HOME/.config/vesktop/themes/catppuccin-mocha.css" "https://raw.githubu
 # Update desktop entry
 echo "Updating Vesktop desktop entry (name change to Discord)..."
 sudo sed -i 's/^Name=Vesktop$/Name=Discord/' /usr/share/applications/vesktop.desktop
+sudo wget -O '/usr/share/applications/discord.png' "https://static.wikia.nocookie.net/logopedia/images/d/dd/Discord_2021_Alt1.svg/revision/latest?cb=20220528092716"
+sudo sed -i 's|^Icon=vesktop$|Icon=/usr/share/applications/discord.png|' /usr/share/applications/vesktop.desktop
+gtk-update-icon-cache /usr/share/icons/*
