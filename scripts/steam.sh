2b405acf68ab63e494de93bf17e0d75527e4377e
furglitch <samlchmn@gmail.com>
2025-03-25 16:38:10 -0500

    Adjust Steam script

--- scripts/steam.sh
+++ scripts/steam.sh
@@ -49,16 +49,11 @@ function fixDesktop { # Fixes an issue with the Steam desktop file
 }
 
 if ! command -v steam 2>&1 /dev/null; then
-    zenity --question --text="Would you like to install Steam?" --title=".dotfiles setup - Steam" 
-    if [ $? -eq 0 ]; then
-        echo "Installing Steam..."
-        yay -Sy steam --noconfirm
-        echo "Steam installed!"
-        fixDesktop
-        themeSteam
-    else
-        echo "Steam will not be installed."
-    fi
+    echo "Installing Steam..."
+    yay -Sy steam --noconfirm
+    echo "Steam installed!"
+    fixDesktop
+    themeSteam
 else
     echo "Steam is already installed."
     themeSteam
