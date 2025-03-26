552fb737e8d60b3df3aa34780343d595e6c0ec46
furglitch <samlchmn@gmail.com>
2025-03-26 02:07:08 -0500

    Add first-time run

--- scripts/discord.sh
+++ scripts/discord.sh
@@ -11,3 +11,9 @@ sudo sed -i 's/^Name=Vesktop$/Name=Discord/' /usr/share/applications/vesktop.des
 sudo wget -O '/usr/share/applications/discord.png' "https://static.wikia.nocookie.net/logopedia/images/d/dd/Discord_2021_Alt1.svg/revision/latest?cb=20220528092716"
 sudo sed -i 's|^Icon=vesktop$|Icon=/usr/share/applications/discord.png|' /usr/share/applications/vesktop.desktop
 gtk-update-icon-cache /usr/share/icons/*
+
+# Run Vesktop to allow for theme installation
+echo "Running Discord to apply theme..."
+echo "This may or may not work." && echo "If it doesn't automatically apply, you can apply it manually by going to the settings and selecting the theme."
+vesktop 2>%1 > /dev/null
+sleep 5s && pkill -f 'vesktop'
