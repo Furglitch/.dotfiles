7620c8a721bff9c16260b1c64eda982ed6fdd121
furglitch <samlchmn@gmail.com>
2025-03-27 11:34:25 -0500

    Adjust kvantum configuration

--- scripts/kvantum.sh
+++ scripts/kvantum.sh
@@ -8,10 +8,3 @@ echo -e "\n[UiSettings]\nColorScheme=kvantum-dark" >> ~/.config/dolphinrc
 if ! sudo sed -i 's/^TerminalProgram=.*$/TerminalProgram=kitty/' ~/.config/kdeglobals; then
     sudo sed -i 's/^\[General\].*$/\[General\]\nTerminalProgram=kitty/' ~/.config/kdeglobals
 fi
-
-kvantummanager &
-
-echo ''
-echo "Kvantum requires manual themeing."
-echo "Under Install/Update Theme - Set the theme folder to $HOME/.dotfiles/themes/kvantum/catppuccin-mocha-sky"
-echo "Under Change/Delete Theme  - Select 'catppuccin-mocha-sky' and click 'Use this theme'"
