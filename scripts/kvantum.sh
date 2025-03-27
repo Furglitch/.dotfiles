3b6335d511b89d8de9e1e0e07f47936f9c430a36
furglitch <samlchmn@gmail.com>
2025-03-26 20:54:18 -0500

    Add script to change default terminal on dolphin

--- scripts/kvantum.sh
+++ scripts/kvantum.sh
@@ -1,4 +1,14 @@
+# Apply theme to KDE Plasma applications
+sudo sed -i 's/^widgetStyle=.*$/widgetStyle=kvantum-dark/' ~/.config/kdeglobals
+
+# Apply theme to Dolphin
 echo -e "\n[UiSettings]\nColorScheme=kvantum-dark" >> ~/.config/dolphinrc
+
+# Set Dolphin to use kitty on 'Open With...'
+if ! sudo sed -i 's/^TerminalProgram=.*$/TerminalProgram=kitty/' ~/.config/kdeglobals; then
+    sudo sed -i 's/^\[General\].*$/\[General\]\nTerminalProgram=kitty/' ~/.config/kdeglobals
+fi
+
 kvantummanager &
 
 echo ''
