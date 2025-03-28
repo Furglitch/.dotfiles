7836775c1e331612eaec703c05ad0088be0aa437
furglitch <samlchmn@gmail.com>
2025-03-28 11:01:31 -0500

    Change install order

--- scripts/install.sh
+++ scripts/install.sh
@@ -23,9 +23,9 @@ done
 
 bash $HOME/.dotfiles/scripts/modules/title.sh
 bash $HOME/.dotfiles/scripts/modules/packages.sh $silent $spin
-bash $HOME/.dotfiles/scripts/modules/symlinks.sh $silent
-bash $HOME/.dotfiles/scripts/modules/editor.sh $silent
 bash $HOME/.dotfiles/scripts/modules/steam.sh $silent $spin
 bash $HOME/.dotfiles/scripts/modules/librewolf.sh $silent $spin
 bash $HOME/.dotfiles/scripts/modules/vscode.sh $silent $spin
+bash $HOME/.dotfiles/scripts/modules/symlinks.sh $silent
+bash $HOME/.dotfiles/scripts/modules/editor.sh $silent
 bash $HOME/.dotfiles/scripts/modules/reboot.sh
