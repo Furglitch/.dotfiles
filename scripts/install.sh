4ca76662d1e3c95dab00fb509ac5c5c6e8aecce7
Furglitch <78628049+Furglitch@users.noreply.github.com>
2025-03-28 23:45:33 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -21,11 +21,11 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
-bash $HOME/.dotfiles/scripts/modules/title.sh
-bash $HOME/.dotfiles/scripts/modules/packages.sh $silent $spin
-bash $HOME/.dotfiles/scripts/modules/steam.sh $silent $spin
-bash $HOME/.dotfiles/scripts/modules/librewolf.sh $silent $spin
-bash $HOME/.dotfiles/scripts/modules/vscode.sh $silent $spin
-bash $HOME/.dotfiles/scripts/modules/symlinks.sh $silent
-bash $HOME/.dotfiles/scripts/modules/editor.sh $silent
-bash $HOME/.dotfiles/scripts/modules/reboot.sh
+$HOME/.dotfiles/scripts/modules/title.sh
+$HOME/.dotfiles/scripts/modules/packages.sh $silent $spin
+$HOME/.dotfiles/scripts/modules/steam.sh $silent $spin
+$HOME/.dotfiles/scripts/modules/librewolf.sh $silent $spin
+$HOME/.dotfiles/scripts/modules/vscode.sh $silent $spin
+$HOME/.dotfiles/scripts/modules/symlinks.sh $silent
+$HOME/.dotfiles/scripts/modules/editor.sh $silent
+$HOME/.dotfiles/scripts/modules/reboot.sh
