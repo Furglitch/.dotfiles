a503a7bdfc347ef480330dbbfb8343fe8910e484
Furglitch <samsonger@proton.me>
2026-06-24 15:32:55 -0500

    role: wayle

--- roles/hyprland/files/hyprland/config/modules/autostart.lua
+++ roles/hyprland/files/hyprland/config/modules/autostart.lua
@@ -5,7 +5,7 @@ end)
 
 -- Desktop
 hl.on("hyprland.start", function()
-    hl.exec_cmd("hyprpanel")
+    hl.exec_cmd("wayle panel start")
     hl.exec_cmd("hyprpaper")
     hl.exec_cmd("waypaper --random")
     hl.exec_cmd("vicinae server")
